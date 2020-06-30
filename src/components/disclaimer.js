91387f507676506f1cf2b9ce024d8488e7661b77
Nicolas <nicolas.charvoz@dashlane.com>
2020-06-30 11:54:56 +0200

    Product Hunt badge

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -1,11 +1,12 @@
 import React from "react"
-import ExternalLink from "../components/externalLink"
+import ProductHunt from "../components/producthunt"
 
 class Disclaimer extends React.Component {
     render() {
         return (
             <footer class="footer">
                 <div class="content has-text-centered">
+                <ProductHunt />
                     <p>
                         <strong>iOS Developer.life</strong> by <a href="https://twitter.com/NicolasCharvoz">Nicolas Charvoz</a>. Based on an original idea by <a href="https://twitter.com/florianmari">Florian Mari</a><br/>
                         © No-Copyright
---
+++ src/components/producthunt.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <a href="https://www.producthunt.com/posts/ios-developer-life?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-ios-developer-life" target="_blank">
+    <img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=211155&theme=light" alt="iOS Developer Life - Guidelines on how to become a better iOS Developer | Product Hunt Embed"/>
+  </a>
+)
